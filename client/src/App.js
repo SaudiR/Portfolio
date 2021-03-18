e34fea155140ea726c2dc93b5ccf9965b8fd1ac2
SaudiR <saudirodriguez20@gmail.com>
2021-03-18 01:35:31 -0400

    file structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Route } from 'react-router';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Saudi Rodriguez</h1>
+      
+    </div>
+  );
+}
+
+export default App;
